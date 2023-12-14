04347871b1e295d26ed56e95261072d31a97270f
Tommy3DE <84966413+Tommy3DE@users.noreply.github.com>
2023-12-14 13:23:45 +0100

    implementation of dayjs and development of scanperiod

--- src/loggedUser/ScanEmail/ScanPeriod.tsx
+++ src/loggedUser/ScanEmail/ScanPeriod.tsx
@@ -1,16 +1,42 @@
+import { useEffect, useState } from "react";
 import SlimNav from "../../layout/SlimNav";
+import * as dayjs from "dayjs";
 
 const ScanPeriod = () => {
+  const [date, setDate] = useState("");
+
+  useEffect(() => {
+    const today = dayjs().format("YYYY-MM-DD HH:mm");
+    setDate(today);
+  }, []);
   return (
     <section className=" font-poppins">
       <SlimNav />
-      <div className="mt-36 lg:mx-[10%] mx-[2%]">
-          <h1 className="text-4xl">
+      <div className="mt-36 lg:mx-[10%] mx-[2%] flex flex-col justifty-center items-center">
+        <h1 className="text-4xl font-black">
           Wybierz miesiąc który chcesz przeskanować:
-          </h1>
-      </div>
-      <div>
-    
+        </h1>
+        <div className="w-2/3">
+        <div className="flex flex-row justify-between items-center mt-16 text-2xl">
+          <p className="">Dziś jest:</p>
+          <input
+            type="text"
+            className="rounded-lg w-1/3 p-2  bg-gray-300"
+            disabled
+            value={date}
+          />
+        </div>
+        <div className="flex flex-row justify-between items-center mt-16 text-2xl">
+          <p className="">Ostatenie skanowanie miało miejsce:</p>
+          <input
+            type="text"
+            className="rounded-lg w-1/3 p-2  bg-gray-300"
+            disabled
+            value={'nigdy'}
+          />
+        </div>
+        </div>
+        
       </div>
     </section>
   );
