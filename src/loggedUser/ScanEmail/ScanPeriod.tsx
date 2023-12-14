780538bf34fda034b1060447585f5478c89ff955
Tommy3DE <84966413+Tommy3DE@users.noreply.github.com>
2023-12-14 20:16:18 +0100

    scanning page date and changes to buttons

--- src/loggedUser/ScanEmail/ScanPeriod.tsx
+++ src/loggedUser/ScanEmail/ScanPeriod.tsx
@@ -1,14 +1,17 @@
 import { useEffect, useState } from "react";
 import SlimNav from "../../layout/SlimNav";
 import * as dayjs from "dayjs";
+import { Link } from "react-router-dom";
 
 const ScanPeriod = () => {
   const [date, setDate] = useState("");
+  const [selectedPeriod, setSelectedPeriod] = useState("");
 
   useEffect(() => {
     const today = dayjs().format("YYYY-MM-DD HH:mm");
     setDate(today);
   }, []);
+
   return (
     <section className=" font-poppins">
       <SlimNav />
@@ -17,29 +20,57 @@ const ScanPeriod = () => {
           Wybierz miesiąc który chcesz przeskanować:
         </h1>
         <div className="w-2/3">
-        <div className="flex flex-row justify-between items-center mt-16 text-2xl">
-          <p className="">Dziś jest:</p>
-          <input
-            type="text"
-            className="rounded-lg w-1/3 p-2  bg-gray-300"
-            disabled
-            value={date}
-          />
+          <div className="flex flex-row justify-between items-center mt-16 text-2xl">
+            <p className="">Dziś jest:</p>
+            <input
+              type="text"
+              className="rounded-lg w-1/3 p-2  bg-gray-300"
+              disabled
+              value={date}
+            />
+          </div>
+          <div className="flex flex-row justify-between items-center mt-16 text-2xl">
+            <p className="">Ostatenie skanowanie miało miejsce:</p>
+            <input
+              type="text"
+              className="rounded-lg w-1/3 p-2  bg-gray-300"
+              disabled
+              value={"nigdy"}
+            />
+          </div>
         </div>
-        <div className="flex flex-row justify-between items-center mt-16 text-2xl">
-          <p className="">Ostatenie skanowanie miało miejsce:</p>
-          <input
-            type="text"
-            className="rounded-lg w-1/3 p-2  bg-gray-300"
-            disabled
-            value={'nigdy'}
-          />
+        <div className="mt-16">
+          <label htmlFor="month-picker" className="text-2xl mr-4">
+            Wybierz miesiąc:
+          </label>
+          <select
+            id="month-picker"
+            className="rounded-lg p-2 bg-gray-300 text-2xl"
+            onChange={(e) => setSelectedPeriod(e.target.value)}
+          >
+            <option value="">--Wybierz--</option>
+            <option value="Styczeń">Styczeń</option>
+            <option value="Luty">Luty</option>
+            <option value="Marzec">Marzec</option>
+            <option value="Kwiecień">Kwiecień</option>
+            <option value="Maj">Maj</option>
+            <option value="Czerwiec">Czerwiec</option>
+            <option value="Lipiec">Lipiec</option>
+            <option value="Sierpień">Sierpień</option>
+            <option value="Wrzesień">Wrzesień</option>
+            <option value="Październik">Październik</option>
+            <option value="Listopad">Listopad</option>
+            <option value="Grudzień">Grudzień</option>
+          </select>
         </div>
-        </div>
-        <button className="w-1/3 bg-[#B1FFD0] rounded px-4 py-2 text-3xl mt-4 shadow-xl">Bierzący miesiąc</button>
-        <button className="w-1/3 bg-[#B1FFD0] rounded px-4 py-2 text-3xl my-4 shadow-xl">Grudzień</button>
-        <button className="w-1/3 bg-[#B1FFD0] rounded px-4 py-2 text-3xl shadow-xl">Styczeń</button>
-
+        {selectedPeriod !== "" && (
+          <Link
+            to="/logged/scanMail/scanPeriod/scannedDocs"
+            className="mt-16 text-3xl"
+          >
+            <button>Dalej</button>
+          </Link>
+        )}
       </div>
     </section>
   );
