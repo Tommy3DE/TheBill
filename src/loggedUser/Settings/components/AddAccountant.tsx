31257d04cb042fed3aa48a585814b4463c001f80
Tommy3DE <84966413+Tommy3DE@users.noreply.github.com>
2023-12-21 13:19:16 +0100

    modal ready

--- src/loggedUser/Settings/components/AddAccountant.tsx
+++ src/loggedUser/Settings/components/AddAccountant.tsx
@@ -3,16 +3,24 @@ type visType = {
 } 
 
 const AddAccountant = ({handleVis}:visType) => {
+    const handleBoardClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        e.stopPropagation();
+    }
   return (
     <div className="h-full w-full bg-gray-600 bg-opacity-80 absolute left-0 top-0 z-10" onClick={handleVis}>
-        <div className="relative top-[25%] w-[60%] bg-white z-20 left-[20%] h-auto rounded-lg">
-            <div className="flex flex-row text-xl">
-                <label htmlFor="name">Imie:</label>
-                <input type="text" id='name' />
+        <div className="relative top-[35%] w-[30%] bg-white z-20 left-[35%] p-5 h-auto rounded-lg" onClick={handleBoardClick}>
+            <div className="flex flex-row text-xl justify-between">
+                <label htmlFor="name">Nazwa:</label>
+                <input type="text" id='name' className="bg-gray-200 rounded-md" placeholder="np. Księgowośc Nowak sp. z o.o."/>
             </div>
-            <div className="flex flex-row text-xl">
+            <div className="flex flex-row text-xl mt-5  justify-between">
                 <label htmlFor="email">Email:</label>
-                <input type="email" id='email' />
+                <input type="email" id='email' className="bg-gray-200 rounded-md" placeholder='np. jan@nowak.pl'/>
+            </div>
+            <div className='flex justify-end'>
+            <button className="mt-5 px-5 py-3 rounded-lg bg-green-700 text-xl text-white font-extrabold hover:scale-105">
+                Dodaj
+            </button>
             </div>
         </div>
     </div>
