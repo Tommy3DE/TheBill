c31f1bf6813ecbd117a4b5f2ae80822f370ebb01
TomaszKJK <84966413+Tommy3DE@users.noreply.github.com>
2024-01-29 17:21:55 +0100

    styling

--- src/layout/SlimNav.tsx
+++ src/layout/SlimNav.tsx
@@ -5,7 +5,7 @@ const SlimNav = () => {
   return (
 <section className="h-20 w-full flex flex-row justify-start  bg-[#5ffa9d] border-y-4 border-[#1A9367]">
         <Link to="/" className="cursor-pointer">
-          <img src={logo} alt="logo" className="mt-7 lg:ml-10"/>
+          <img src={logo} alt="logo" className="mt-7 lg:ml-10 h-40"/>
         </Link>
     </section>
   );
