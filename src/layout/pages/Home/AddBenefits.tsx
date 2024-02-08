cb2afb344828a27ed226e9d3be1ee19dbaa59915
TomaszKJK <84966413+Tommy3DE@users.noreply.github.com>
2024-02-09 00:18:11 +0530

    benefits dektop ready

--- src/layout/pages/Home/AddBenefits.tsx
+++ src/layout/pages/Home/AddBenefits.tsx
@@ -1,8 +1,8 @@
 import { ReactNode } from "react";
-import doc from '../../../assets/addBenefits/Documents-bro 1.png'
-import contact from '../../../assets/addBenefits/Contact us-bro 1.png'
-import conversion from '../../../assets/addBenefits/Conversion rate optimization-rafiki 1.png'
-import men from '../../../assets/addBenefits/Contact us-bro 1.png'
+import doc from "../../../assets/addBenefits/Documents-bro 1.png";
+import contact from "../../../assets/addBenefits/Contact us-bro 1.png";
+import conversion from "../../../assets/addBenefits/Conversion rate optimization-rafiki 1.png";
+import men from "../../../assets/addBenefits/Contact us-bro 1.png";
 
 interface BenefitTile {
   id: number;
@@ -21,52 +21,75 @@ const AddBenefits = () => {
       img: doc,
       text: (
         <>
-          <span className="font-black">OneBill</span>podczas skanowania Twojej
+          <span className="font-black">OneBill</span> podczas skanowania Twojej
           skrzynki e-mail, organizuje dokumenty kosztowe zgodnie z datą
           sprzedaży i umieszcza w odpowiednich folderach.
         </>
       ),
     },
     {
-        id: 2,
-        title: "Usprawnia komunikację z biurem księgowym",
-        subtitle: "Usprawnij komunikację",
-        img: contact,
-        text: (
-          <>
-            OneBill podczas skanowania przygotowuje paczki dokumentów, które w 2 krokach mogą być wysłane jednym mailem do księgowego (w ZIP lub PDF)
-          </>
-        ),
-      },
-      {
-        id: 3,
-        title: "Koniec z brakującymi fakturami!",
-        subtitle: "Uszczelnij koszty",
-        img: conversion,
-        text: (
-          <>
-            Dzięki zaawansowanemu algorytmowi skanowania, będziesz miał pewność ile faktur trafiło do księgowego. Będziesz miał również wgląd do historii skanowania swojej skrzynki.
-          </>
-        ),
-      },
-      {
-        id: 4,
-        title: "Optymalizuj swoje podatki",
-        subtitle: "Kontroluj wydatki",
-        img: men,
-        text: (
-          <>
-            Z nami będziesz miał pod kontrolą wszystkie faktury kosztowe, co przełoży się na większe odliczenia kosztów - mniej niezaliczonych faktur.
-          </>
-        ),
-      },
+      id: 2,
+      title: "Usprawnia komunikację z biurem księgowym",
+      subtitle: "Usprawnij komunikację",
+      img: contact,
+      text: (
+        <>
+          OneBill podczas skanowania przygotowuje paczki dokumentów, które w 2
+          krokach mogą być wysłane jednym mailem do księgowego (w ZIP lub PDF)
+        </>
+      ),
+    },
+    {
+      id: 3,
+      title: "Koniec z brakującymi fakturami!",
+      subtitle: "Uszczelnij koszty",
+      img: conversion,
+      text: (
+        <>
+          Dzięki zaawansowanemu algorytmowi skanowania, będziesz miał pewność
+          ile faktur trafiło do księgowego. Będziesz miał również wgląd do
+          historii skanowania swojej skrzynki.
+        </>
+      ),
+    },
+    {
+      id: 4,
+      title: "Optymalizuj swoje podatki",
+      subtitle: "Kontroluj wydatki",
+      img: men,
+      text: (
+        <>
+          Z nami będziesz miał pod kontrolą wszystkie faktury kosztowe, co
+          przełoży się na większe odliczenia kosztów - mniej niezaliczonych
+          faktur.
+        </>
+      ),
+    },
   ];
   return (
-    <div className=" my-8 mx-[8%] font-poppins">
+    <div className=" my-8 w-full font-poppins">
       <h1 className="my-8 text-5xl font-playFair bg-[#83F9CE] bg-opacity-40 py-6 font-bold text-center rounded-md backdrop-blur-md">
         Dodatkowe korzyści jakie oferuje OneBill
       </h1>
-      <div className="bg-gray-300"></div>
+      <div className="bg-gray-100 py-10 font-poppins">
+        {tiles.map((tile) => (
+          <div
+            key={tile.id}
+            className={`flex mx-40 justify-center items-center ${
+              tile.id === 1 || tile.id === 3 ? "flex-row-reverse" : "flex-row"
+            }`}
+          >
+            <div className="flex items-center flex-col justify-center">
+              <img src={tile.img} alt="pic" />
+              <h3 className="font-playFair -my-2 font-black text-xl">{tile.subtitle}</h3>
+            </div>
+            <div className=" w-5/12">
+              <h4 className="text-2xl text-[#1A9367]">{tile.title}</h4>
+              <h3>{tile.text}</h3>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
