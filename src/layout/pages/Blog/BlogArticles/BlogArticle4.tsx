0f6b0178d1087c9ed80734cad34211227af034be
TomaszKJK <84966413+Tommy3DE@users.noreply.github.com>
2024-02-25 08:58:36 +0100

    art 4

--- src/layout/pages/Blog/BlogArticles/BlogArticle4.tsx
+++ src/layout/pages/Blog/BlogArticles/BlogArticle4.tsx
@@ -5,8 +5,8 @@ import { FaArrowRight } from "react-icons/fa";
 import fb from "../../../../assets/socialMedia/fb.png";
 import x from "../../../../assets/socialMedia/x.png";
 import ig from "../../../../assets/socialMedia/ig.png";
-import pic2 from "../../../../assets/articlesThumbnail/simon-kadula-8gr6bObQLOI-unsplash 1.png";
-import pic4 from "../../../../assets/articlesThumbnail/sajad-nori-oPRU2Cne0Pw-unsplash 1.png";
+import pic1 from "../../../../assets/articlesThumbnail/image 1.png";
+import pic2 from "../../../../assets/articlesThumbnail/austin-distel-rxpThOwuVgE-unsplash 1.png";
 
 const BlogArticle4 = () => {
   return (
@@ -14,79 +14,81 @@ const BlogArticle4 = () => {
       <Navbar />
       <div className="max-w-[1980px] mt-32 mx-[8%]">
         <h1 className=" font-playFair text-4xl font-black text-[#1A9367]">
-          Najważniejsze daty w roku rozliczeniowym - o czym pamiętać?{" "}
+          Cyfryzacja Twojej firmy - od papierkowej dokumentacji do zarządzania
+          online{" "}
         </h1>
         <div className="font-poppins text-gray-600">
           <p className="my-8">
-            Prowadzenie firmy w Polsce wiąże się z szeregiem terminowych
-            obowiązków, których należy dopełniać, aby działać zgodnie z prawem i
-            unikać nieprzyjemnych konsekwencji podatkowych. W niniejszym
-            artykule omówimy kluczowe terminy i obowiązki polskich
-            przedsiębiorców, aby pomóc w zrozumieniu i zaplanowaniu działań.
+            W dzisiejszym szybko rozwijającym się świecie biznesu, cyfryzacja
+            stała się kluczowa dla efektywności i wzrostu. Przejście od
+            tradycyjnych, papierowych metod zarządzania do cyfryzacji procesów
+            biznesowych może wydawać się wyzwaniem, ale jest niezbędne dla
+            nowoczesnego przedsiębiorstwa.
             <br />
-            <br />W tym artykule, przedstawimy podstawowe terminy, jakich polski
-            przedsiębiorca musi wpisać w swój kalendarz.
+            <br />W tym artykule dowiesz się, jak skutecznie dokonać tej
+            transformacji.
           </p>
-          <p className="font-black text-[#1A9367] mt-5">Podatek Dochodowy </p>
+          <p className="font-black text-[#1A9367] mt-5">Korzyści Cyfryzacji</p>
           <p>
-            Podatek dochodowy jest jednym z kluczowych obowiązków każdego
-            przedsiębiorcy. W Polsce istnieją różne formy opodatkowania, takie
-            jak opodatkowanie na zasadach ogólnych lub podatek liniowy. Zaliczki
-            na podatek dochodowy należy opłacać zgodnie z ustalonym
-            harmonogramem:
-            <br />- Stawka podatku PIT różni się w zależności od posiadanej
-            formy opodatkowania:
-            <br />- Rozliczenie miesięczne: do 20. dnia miesiąca następującego
-            po rozliczanym miesiącu.
-            <br />- Rozliczenie kwartalne: do 20. dnia miesiąca następującego po
-            rozliczanym kwartale.
-          </p>
-          <p className="font-black text-[#1A9367] mt-5">Podatek VAT </p>
-          <p>
-            Przedsiębiorcy zarejestrowani jako podatnicy VAT mają obowiązek
-            opłacania podatku VAT w ustalonych terminach:
-            <br />- Aktualna stawka podatku VAT: 23 %
-            <br />- Rozliczenie miesięczne: do 25. dnia miesiąca następującego
-            po rozliczanym miesiącu.
-            <br />- Rozliczenie kwartalne: do 25. dnia miesiąca następującego po
-            rozliczanym kwartale.
-          </p>
-          <p className="font-black text-[#1A9367] mt-5">Składki ZUS </p>
-          <p>
-            Opłacanie składek ZUS jest konieczne dla każdego przedsiębiorcy.
-            Terminy opłat są uzależnione od tego, czy przedsiębiorca zatrudnia
-            pracowników na umowy ze składkami ZUS:
-            <br />- Przedsiębiorca niezatrudniający pracowników: do 10. dnia
-            miesiąca.
-            <br />- Przedsiębiorca zatrudniający pracowników: do 15. dnia
-            miesiąca.
+            Słowa kluczowe: efektywność operacyjna, redukcja kosztów, dostęp do
+            danych.
+            <br />
+            Cyfryzacja procesów biznesowych oferuje wiele korzyści, w tym
+            zwiększoną efektywność operacyjną, redukcję kosztów i lepszy dostęp
+            do danych. Przechodząc na zarządzanie online, Twoja firma może
+            znacznie przyspieszyć swoje działania i zwiększyć swoją
+            konkurencyjność na rynku.
           </p>
           <p className="font-black text-[#1A9367] mt-5">
-            Przejście z "Małego ZUS-u" na "Duży ZUS"{" "}
+            Identyfikacja Obszarów do Cyfryzacji
           </p>
           <p>
-            Przedsiębiorcy korzystający z "Małego ZUS-u" muszą dopełnić
-            określonych obowiązków po jego zakończeniu, włączając w to złożenie
-            odpowiednich formularzy w ZUS-ie.
+            Słowa kluczowe: automatyzacja procesów, cyfrowe narzędzia księgowe,
+            zarządzanie dokumentacją. <br />
+            Pierwszym krokiem jest identyfikacja obszarów w Twojej firmie, które
+            mogą skorzystać na cyfryzacji. Czy to księgowość, zarządzanie
+            dokumentacją czy komunikacja wewnętrzna - każdy aspekt działalności
+            może zostać ulepszony przez wprowadzenie odpowiednich cyfrowych
+            narzędzi.
           </p>
           <p className="font-black text-[#1A9367] mt-5">
-            Zmiana formy opodatkowania{" "}
+            Wybór Odpowiednich Narzędzi
           </p>
           <p>
-            Przedsiębiorca musi pamiętać, że na początku każdego roku
-            kalendarzowego, może zmienić swoją formę opodatkowania. To znaczy,
-            jeśli przedsiębiorca swój pierwszy przychód osiągnął w styczniu
-            2024, to czas na zmianę formy opodatkowania ma do 20 lutego 2024.
+            Słowa kluczowe: oprogramowanie do zarządzania firmą, cyfrowe
+            rozwiązania. <br />
+            Wybór odpowiednich narzędzi cyfrowych jest kluczowy. Na rynku
+            dostępne są różnorodne oprogramowania do zarządzania firmą, które
+            mogą pomóc w cyfryzacji różnych aspektów Twojej działalności. Ważne,
+            aby wybrać rozwiązania, które najlepiej odpowiadają potrzebom i
+            specyfice Twojej firmy.
+          </p>
+          <p className="font-black text-[#1A9367] mt-5">
+            Implementacja i Szkolenie
+          </p>
+          <p>
+            Słowa kluczowe: wdrożenie cyfrowych rozwiązań, szkolenie
+            pracowników. <br />
+            Po wybraniu odpowiednich narzędzi, następuje etap wdrożenia.
+            Kluczowe jest tutaj szkolenie pracowników, aby mogli oni efektywnie
+            korzystać z nowych systemów. Pamiętaj, że sukces w cyfryzacji zależy
+            od zaangażowania całego zespołu.
+          </p>
+          <p className="font-black text-[#1A9367] mt-5">
+            Monitorowanie i Optymalizacja{" "}
+          </p>
+          <p>
+            Słowa kluczowe: analiza efektywności, ciągła optymalizacja. <br />
+            Po wdrożeniu cyfrowych rozwiązań, ważne jest ciągłe monitorowanie
+            ich efektywności i dokonywanie niezbędnych optymalizacji. Cyfryzacja
+            to proces ciągły, wymagający regularnych aktualizacji i dostosowań.
           </p>
           <p className=" mt-5">
-            Prowadzenie firmy w Polsce wymaga ścisłego przestrzegania terminów i
-            obowiązków podatkowych. Zrozumienie kluczowych terminów oraz
-            terminowego dopełniania obowiązków podatkowych jest kluczowe dla
-            zapewnienia prawidłowego funkcjonowania firmy i uniknięcia
-            ewentualnych kłopotów z organami podatkowymi. Dlatego też, dbałość o
-            terminowe rozliczenia oraz bieżącą aktualizację wiedzy na temat
-            przepisów podatkowych jest niezwykle istotna dla każdego polskiego
-            przedsiębiorcy.
+            Cyfryzacja jest nieuniknionym kierunkiem rozwoju dla każdej
+            nowoczesnej firmy. Przejście od papierowej dokumentacji do
+            zarządzania online otwiera nowe możliwości dla wzrostu i
+            efektywności Twojej działalności. Pamiętaj, że kluczem do sukcesu
+            jest strategiczne podejście i wybór odpowiednich narzędzi.
           </p>
         </div>
       </div>
@@ -95,13 +97,13 @@ const BlogArticle4 = () => {
       </div>
       <div className="flex flex-row justify-center">
         <a
-          href="https://www.facebook.com/sharer/sharer.php?u=http://onebill.com.pl/blog/3"
+          href="https://www.facebook.com/sharer/sharer.php?u=http://onebill.com.pl/blog/4"
           target="_blank"
         >
           <img src={fb} alt="Udostępnij na Facebooku" className=" h-16" />
         </a>
         <a
-          href="https://twitter.com/intent/tweet?url=http://onebill.com.pl/blog/3&text=Polecam"
+          href="https://twitter.com/intent/tweet?url=http://onebill.com.pl/blog/4&text=Polecam"
           target="_blank"
           className="mx-10"
         >
@@ -114,32 +116,31 @@ const BlogArticle4 = () => {
       </div>
       <div className="mt-14 flex flex-row justify-around font-poppins">
         <div className=" shadow-2xl flex flex-col justify-between w-1/3">
-          <img src={pic2} alt="thumb1" />
+          <img src={pic1} alt="thumb1" />
           <p className="text-lg font-black mx-[3%] my-5">
-            Najważniejsze daty w roku rozliczeniowym - o czym pamiętać?
+            Analiza przypadków użycia OneBill...{" "}
           </p>
           <p className="mx-[3%] ">
-            Każdy przedsiębiorca w Polsce musi pamiętać o kluczowych datach w
-            roku podatkowym, aby uniknąć kar i opóźnień.{" "}
+            Narzędzie do zarządzania kosztami dla mikroprzedsiębiorców.
           </p>
           <Link
-            to={`/blog/3`}
+            to={`/blog/1`}
             className="flex flex-row items-center font-bold mt-5 hover:text-green-700 border-t-2 py-3 mx-[3%]"
           >
             Czytaj dalej <FaArrowRight className="ml-1" />
           </Link>
         </div>
         <div className=" shadow-2xl flex flex-col justify-between w-1/3">
-          <img src={pic4} alt="thumb2" />
+          <img src={pic2} alt="thumb2" />
           <p className="text-lg font-black mx-[3%] my-5">
-            Cyfryzacja Twojej firmy
+            Najczęstsze błędy w zarządzaniu finansami małych firm{" "}
           </p>
           <p className="mx-[3%] ">
-            W dzisiejszym szybko rozwijającym się świecie biznesu, cyfryzacja
-            stała się kluczowa dla efektywności i wzrostu.{" "}
+            Zarządzanie finansami to kluczowy aspekt prowadzenia każdej małej
+            firmy.
           </p>
           <Link
-            to={`/blog/4`}
+            to={`/blog/1`}
             className="flex flex-row items-center font-bold mt-5 hover:text-green-700 border-t-2 py-3 mx-[3%]"
           >
             Czytaj dalej <FaArrowRight className="ml-1" />
@@ -151,4 +152,4 @@ const BlogArticle4 = () => {
   );
 };
 
-export default BlogArticle4
+export default BlogArticle4;
