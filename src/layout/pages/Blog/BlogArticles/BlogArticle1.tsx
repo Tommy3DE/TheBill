ea14f4664757b17b6dbb56f926e0c9ddaa81968f
TomaszKJK <84966413+Tommy3DE@users.noreply.github.com>
2024-02-25 08:40:41 +0100

    art2

--- src/layout/pages/Blog/BlogArticles/BlogArticle1.tsx
+++ src/layout/pages/Blog/BlogArticles/BlogArticle1.tsx
@@ -1,9 +1,12 @@
 import Footer from "../../../Footer";
 import Navbar from "../../../Navbar";
-import pic2 from "../../../../assets/articlesThumbnail/simon-kadula-8gr6bObQLOI-unsplash 1.png";
-import pic3 from "../../../../assets/articlesThumbnail/towfiqu-barbhuiya-bwOAixLG0uc-unsplash 1.png";
 import { Link } from "react-router-dom";
 import { FaArrowRight } from "react-icons/fa";
+import fb from "../../../../assets/socialMedia/fb.png";
+import x from "../../../../assets/socialMedia/x.png";
+import ig from "../../../../assets/socialMedia/ig.png";
+import pic2 from "../../../../assets/articlesThumbnail/simon-kadula-8gr6bObQLOI-unsplash 1.png";
+import pic3 from "../../../../assets/articlesThumbnail/towfiqu-barbhuiya-bwOAixLG0uc-unsplash 1.png";
 
 const BlogArticle1 = () => {
   return (
@@ -80,9 +83,20 @@ const BlogArticle1 = () => {
         Udostępnij
       </div>
       <div className="flex flex-row justify-center">
-        <img src="" alt="fb" />
-        <img src="" alt="x" className="mx-10" />
-        <img src="" alt="insta" />
+        <a
+          href="https://www.facebook.com/sharer/sharer.php?u=http://onebill.com.pl/blog/1"
+          target="_blank"
+        >
+          <img src={fb} alt="Udostępnij na Facebooku" className=" h-16" />
+        </a>
+        <a
+          href="https://twitter.com/intent/tweet?url=http://onebill.com.pl/blog/1&text=Polecam"
+          target="_blank"
+          className="mx-10"
+        >
+          <img src={x} alt="Tweetnij"  className=" h-16"/>
+        </a>{" "}
+        <img src={ig} alt="insta" className=" h-16" />
       </div>
       <div className="text-center my-14 font-playFair font-black text-gray-700 text-3xl">
         Te artykuły mogą Cię zainteresować
@@ -90,20 +104,35 @@ const BlogArticle1 = () => {
       <div className="mt-14 flex flex-row justify-around font-poppins">
         <div className=" shadow-2xl flex flex-col justify-between w-1/3">
           <img src={pic2} alt="thumb1" />
-          <p className="text-lg font-black mx-[3%] my-5">Najważniejsze daty w roku rozliczeniowym - o czym pamiętać?</p>
+          <p className="text-lg font-black mx-[3%] my-5">
+            Najważniejsze daty w roku rozliczeniowym - o czym pamiętać?
+          </p>
           <p className="mx-[3%] ">
             Każdy przedsiębiorca w Polsce musi pamiętać o kluczowych datach w
             roku podatkowym, aby uniknąć kar i opóźnień.{" "}
           </p>
           <Link
-                to={`/blog/3`}
-                className="flex flex-row items-center font-bold mt-5 hover:text-green-700"
-              >
-                Czytaj dalej <FaArrowRight className="ml-1" />
-              </Link>
+            to={`/blog/3`}
+            className="flex flex-row items-center font-bold mt-5 hover:text-green-700 border-t-2 py-3 mx-[3%]"
+          >
+            Czytaj dalej <FaArrowRight className="ml-1" />
+          </Link>
         </div>
         <div className=" shadow-2xl flex flex-col justify-between w-1/3">
           <img src={pic3} alt="thumb2" />
+          <p className="text-lg font-black mx-[3%] my-5">
+            Automatyzacja procesów biznesowych w firmach{" "}
+          </p>
+          <p className="mx-[3%] ">
+            W erze cyfryzacji, automatyzacja procesów finansowych stała się
+            kluczowym elementem efektywnego zarządzania małą firmą.
+          </p>
+          <Link
+            to={`/blog/2`}
+            className="flex flex-row items-center font-bold mt-5 hover:text-green-700 border-t-2 py-3 mx-[3%]"
+          >
+            Czytaj dalej <FaArrowRight className="ml-1" />
+          </Link>
         </div>
       </div>
       <Footer />
--- src/layout/pages/Blog/BlogArticles/BlogArticle2.tsx
+++ src/layout/pages/Blog/BlogArticles/BlogArticle2.tsx
@@ -1,8 +1,154 @@
+import Footer from "../../../Footer";
+import Navbar from "../../../Navbar";
+import { Link } from "react-router-dom";
+import { FaArrowRight } from "react-icons/fa";
+import fb from "../../../../assets/socialMedia/fb.png";
+import x from "../../../../assets/socialMedia/x.png";
+import ig from "../../../../assets/socialMedia/ig.png";
+import pic2 from "../../../../assets/articlesThumbnail/simon-kadula-8gr6bObQLOI-unsplash 1.png";
+import pic1 from "../../../../assets/articlesThumbnail/austin-distel-rxpThOwuVgE-unsplash 1.png";
 
 const BlogArticle2 = () => {
   return (
-    <div>BlogArticle2</div>
-  )
-}
+    <div>
+      <Navbar />
+      <div className="max-w-[1980px] mt-32 mx-[8%]">
+        <h1 className=" font-playFair text-4xl font-black text-[#1A9367]">
+          Automatyzacja procesów biznesowych w firmach{" "}
+        </h1>
+        <div className="font-poppins text-gray-600">
+          <p className="my-8">
+            W erze cyfryzacji, automatyzacja procesów finansowych stała się
+            kluczowym elementem efektywnego zarządzania małą firmą. Nie tylko
+            oszczędza czas, ale także minimalizuje ryzyko błędów i zwiększa
+            efektywność. W tym artykule omówimy, jak automatyzacja może
+            przyczynić się do poprawy Twoich finansów i w jaki sposób aplikacja
+            OneBill może w tym pomóc.
+          </p>
+          <p className="font-black text-[#1A9367] mt-5">
+            Zwiększona Efektywność i Oszczędność Czasu{" "}
+          </p>
+          <p>
+            Automatyzacja Procesów: Ręczne wprowadzanie danych jest czasochłonne
+            i podatne na błędy. Automatyzując takie procesy jak fakturowanie,
+            księgowość czy zarządzanie płatnościami, możesz zaoszczędzić cenne
+            godziny, które zamiast na administrację, możesz poświęcić na rozwój
+            biznesu. <br></br>
+            Przykład z OneBill: OneBill automatycznie skanuje skrzynkę e-mailową
+            w poszukiwaniu faktur, sortuje je i przygotowuje do księgowości,
+            znacznie skracając czas potrzebny na te czynności.
+          </p>
+          <p className="font-black text-[#1A9367] mt-5">
+            Redukcja Błędów i Zwiększenie Dokładności{" "}
+          </p>
+          <p>
+            Dokładność Danych: Ręczne przetwarzanie danych często wiąże się z
+            ryzykiem pomyłek. Automatyzacja pomaga w eliminacji błędów, co jest
+            kluczowe dla dokładności finansowej. <br />
+            OneBill w Akcji: Dzięki zastosowaniu zaawansowanych algorytmów,
+            OneBill minimalizuje ryzyko błędów w przetwarzaniu faktur i innych
+            dokumentów.
+          </p>
+          <p className="font-black text-[#1A9367] mt-5">
+            Lepsza Kontrola i Przejrzystość Finansów{" "}
+          </p>
+          <p>
+            Przegląd Finansów w Jednym Miejscu: Automatyzacja finansów daje
+            pełniejszy i łatwiejszy dostęp do danych finansowych, co pozwala na
+            lepszą kontrolę budżetu i cash flow. <br />
+            Funkcjonalności OneBill: Aplikacja umożliwia szybki dostęp do
+            wszystkich przetworzonych dokumentów, co pozwala na bieżące
+            monitorowanie stanu finansów firmy.
+          </p>
+          <p className="font-black text-[#1A9367] mt-5">
+            Optymalizacja Przepływu Pieniężnego{" "}
+          </p>
+          <p>
+            Automatyczne Przypomnienia o Płatnościach: Zapomnienie o terminowych
+            płatnościach może prowadzić do niepotrzebnych opóźnień i kosztów.
+            Automatyzacja przypomnień pomaga utrzymać płynność finansową.{" "}
+            <br></br>
+            Jak Pomaga OneBill: Aplikacja może automatycznie przypominać o
+            nadchodzących terminach płatności, pomagając w utrzymaniu
+            regularności płatności.
+          </p>
+          <p className="font-black text-[#1A9367] mt-5">
+            Łatwiejsze Przestrzeganie Przepisów Podatkowych{" "}
+          </p>
+          <p>
+            Zgodność z Prawem: Automatyzacja ułatwia przestrzeganie
+            zmieniających się przepisów podatkowych i księgowych, redukując
+            ryzyko niezgodności. <br />
+            Rola OneBill: OneBill pomaga w organizacji i archiwizacji
+            dokumentacji zgodnie z obowiązującymi przepisami, co ułatwia
+            rozliczenia podatkowe.
+          </p>
+          <p className=" mt-5">
+            Automatyzacja procesów finansowych to nie tylko oszczędność czasu i
+            pieniędzy, ale także pewność, że każdy aspekt Twoich finansów jest
+            zarządzany efektywnie i precyzyjnie. Aplikacja OneBill stanowi
+            doskonałe narzędzie do osiągnięcia tych celów, oferują
+          </p>
+        </div>
+      </div>
+      <div className="text-center my-14 font-playFair font-black text-gray-700 text-3xl">
+        Udostępnij
+      </div>
+      <div className="flex flex-row justify-center">
+        <a
+          href="https://www.facebook.com/sharer/sharer.php?u=http://onebill.com.pl/blog/2"
+          target="_blank"
+        >
+          <img src={fb} alt="Udostępnij na Facebooku" className=" h-16" />
+        </a>
+        <a
+          href="https://twitter.com/intent/tweet?url=http://onebill.com.pl/blog/2&text=Polecam"
+          target="_blank"
+          className="mx-10"
+        >
+          <img src={x} alt="Tweetnij" className=" h-16" />
+        </a>{" "}
+        <img src={ig} alt="insta" className=" h-16" />
+      </div>
+      <div className="text-center my-14 font-playFair font-black text-gray-700 text-3xl">
+        Te artykuły mogą Cię zainteresować
+      </div>
+      <div className="mt-14 flex flex-row justify-around font-poppins">
+        
+        <div className=" shadow-2xl flex flex-col justify-between w-1/3">
+          <img src={pic1} alt="thumb2" />
+          <p className="text-lg font-black mx-[3%] my-5">
+          Najczęstsze błędy w zarządzaniu finansami małych firm          </p>
+          <p className="mx-[3%] ">
+          Zarządzanie finansami to kluczowy aspekt prowadzenia każdej małej firmy. 
+          </p>
+          <Link
+            to={`/blog/2`}
+            className="flex flex-row items-center font-bold mt-5 hover:text-green-700 border-t-2 py-3 mx-[3%]"
+          >
+            Czytaj dalej <FaArrowRight className="ml-1" />
+          </Link>
+        </div>
+        <div className=" shadow-2xl flex flex-col justify-between w-1/3">
+          <img src={pic2} alt="thumb1" />
+          <p className="text-lg font-black mx-[3%] my-5">
+            Najważniejsze daty w roku rozliczeniowym - o czym pamiętać?
+          </p>
+          <p className="mx-[3%] ">
+            Każdy przedsiębiorca w Polsce musi pamiętać o kluczowych datach w
+            roku podatkowym, aby uniknąć kar i opóźnień.{" "}
+          </p>
+          <Link
+            to={`/blog/3`}
+            className="flex flex-row items-center font-bold mt-5 hover:text-green-700 border-t-2 py-3 mx-[3%]"
+          >
+            Czytaj dalej <FaArrowRight className="ml-1" />
+          </Link>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
-export default BlogArticle2
+export default BlogArticle2;
