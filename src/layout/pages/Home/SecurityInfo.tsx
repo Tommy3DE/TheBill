aa6507783e1023fa6166422bdbd7c80a42bccbdd
TomaszKJK <84966413+Tommy3DE@users.noreply.github.com>
2024-03-29 14:31:19 +0100

    sec ready w/o rwd

--- src/layout/pages/Home/SecurityInfo.tsx
+++ src/layout/pages/Home/SecurityInfo.tsx
@@ -11,6 +11,10 @@ import finish from "../../../assets/securityAssets/secGrats.png";
 import secInv from "../../../assets/securityAssets/secInv.png";
 import secCal from "../../../assets/securityAssets/secCal.png";
 import secScan from "../../../assets/securityAssets/secScan.png";
+import tick from "../../../assets/icons/tick.png";
+import warn from "../../../assets/securityAssets/secWarn.png";
+import priv from '../../../assets/securityAssets/SecPriv.png'
+import point from '../../../assets/securityAssets/info.png'
 
 const DiagonalLines = () => {
   return (
@@ -34,15 +38,15 @@ const SecurityInfo = () => {
           Jak działa One<span className="text-[#1A9367]">Bill</span>?
         </p>
       </div>
-      <div className="flex flex-col justify-center mx-[10%] items-center">
+      <div className="flex flex-col justify-center mx-[10%] items-center my-10">
         <div>
           <div className="flex flex-row justify-center items-center">
             <img src={step1} alt="step1" className="w-24 h-24 " />
-            <h3 className="text-xl ml-10">
+            <h3 className="text-xl mx-10">
               Po zalogowaniu do aplikacji, zostaniesz poproszony, o wyrażenie{" "}
               <span className="font-bold"> zgody w oknie Google.</span>
             </h3>
-            <img src={google} alt=" google" className="h-24" />
+            <img src={google} alt=" google" className="h-20" />
           </div>
           <div className="flex flex-row justify-center">
             <img src={api} alt="googleapi" className="h-36" />
@@ -104,6 +108,71 @@ const SecurityInfo = () => {
           </div>
           <img src={finish} alt="finish" className="mx-auto h-80" />
         </div>
+      </div>
+      <div className="my-5 mx-[15%]">
+        <p className="text-3xl text-center">
+          W One<span className="text-[#1A9367] font-bold">Bill</span>:
+        </p>
+        <div>
+          <div className="text-xl flex mb-5 items-center">
+            <img src={tick} alt="tick" />
+            <h3 className="ml-5">
+              <span className="text-[#1A9367]">Sam decydujesz</span>, które
+              faktury mają zostać zapisane na Twoim koncie a które usunięte po
+              każdym skanowaniu,
+            </h3>
+          </div>
+          <div className="text-xl mb-5 flex items-center">
+            <img src={tick} alt="tick" />
+            <h3 className="ml-5">
+              <span className="text-[#1A9367]">Masz pełną kontrolę </span>
+              nad swoim kontem. W każdej chwili możesz usunąć lub pobrać
+              fakturę,
+            </h3>
+          </div>
+          <div className="text-xl flex items-center">
+            <img src={tick} alt="tick" />
+            <h3 className="ml-5">
+              Nasz program nie przechowuje{" "}
+              <span className="text-[#1A9367]">żadnych innych plików</span>{" "}
+              oprócz faktur (w formacie PDF lub JPG).
+            </h3>
+          </div>
+        </div>
+        <img src={warn} alt="warning" className="mx-auto" />
+        <p className="tracking-wider mx-[5%] text-center">
+          <span className="text-[#EEB524] font-bold">Usunięcie faktury </span>z
+          naszego systemu spowoduje nieodwracalne utracenie jej w naszej
+          aplikacji. Oznaczać to będzie, że należy wykonać{" "}
+          <span className="font-bold underline">
+            ponowny skan w celu wyświetlenia usuniętych dokumentów
+          </span>
+          .
+        </p>
+        <img src={priv} alt="priv" className="mx-auto h-20 my-5"/>
+        <p className="text-3xl text-center">
+        W kwestii zachowania <span className="text-[#1A9367] font-bold">prywatności</span>,  nasz algorytm: :
+        </p>
+        <div className="mt-8">
+          <div className="text-xl flex mb-5 items-center ">
+            <img src={point} alt="point" />
+            <h3 className="ml-5">
+              <span className="font-bold underline">Nie czyta</span>,treści wiadomości e-mail.
+            </h3>
+          </div>
+          <div className="text-xl flex mb-5 items-center ">
+            <img src={point} alt="point" />
+            <h3 className="ml-5">
+              <span className="font-bold underline">Nie wchodzi do wiadomości</span> mailowych a jedynie sprawdza ich załączniki.
+            </h3>
+          </div>
+          <div className="text-xl flex mb-5 items-center ">
+            <img src={point} alt="point" />
+            <h3 className="ml-5">
+              <span className="font-bold underline">Wyszukuje załączniki</span>, które mają postać faktury na twojej skrzynce mailowej.
+            </h3>
+          </div>
+          </div>
       </div>
     </div>
   );
