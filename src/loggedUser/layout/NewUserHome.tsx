dbb3f482974998d35afa216108043983509cc849
TomaszKJK <84966413+Tommy3DE@users.noreply.github.com>
2024-04-02 09:22:57 +0200

    new step

--- src/loggedUser/layout/NewUserHome.tsx
+++ src/loggedUser/layout/NewUserHome.tsx
@@ -1,6 +1,7 @@
 import { FormEvent, useState } from "react";
 import sync from "../../assets/iconsLogged/sync.png";
 import accountant from "../../assets/accountant.png";
+import calendar from "../../assets/calendar.png";
 import { toast } from "react-toastify";
 
 const NewUserHome = () => {
@@ -13,10 +14,10 @@ const NewUserHome = () => {
   const handleAddAcc = async (e: FormEvent) => {
     e.preventDefault();
     const accessToken = localStorage.getItem("accessToken");
-  const requestBody = {
-    name: name,
-    email: mail,
-  };
+    const requestBody = {
+      name: name,
+      email: mail,
+    };
 
     fetch("https://api.onebill.com.pl/api/bookkeeper", {
       method: "Post",
@@ -32,14 +33,14 @@ const NewUserHome = () => {
             position: "top-right",
             autoClose: 7000,
           });
-          handleNextStep()
+          handleNextStep();
         } else {
           toast.error("Błąd dodawania biura księgowego", {
             position: "top-right",
             autoClose: 7000,
           });
         }
-        
+
         return response.json();
       })
       .catch((error) => console.error("Error:", error));
@@ -53,7 +54,7 @@ const NewUserHome = () => {
             Witaj! Zacznimy od połączenia Twojej skrzynki mailowej z kontem{" "}
             <span className="text-[#1A9367]">OneBill</span>
           </p>
-          <p className="my-8">
+          <p className="my-8 text-xl text-gray-500">
             Dzięki temu będziesz mógł skanować swoją skrzynkę w poszukiwaniu
             faktur.
           </p>
@@ -71,19 +72,19 @@ const NewUserHome = () => {
           <p className="text-3xl font-bold tracking-wider font-poppins">
             Dodaj dane biura księgowego
           </p>
-          <p className="my-8">
+          <p className="my-8 text-xl text-gray-500">
             Dzięki temu będziemy wiedzieć do kogo przesyłać Twoje faktury.
           </p>
           <input
             type="text"
             className="bg-gray-100 mb-8 px-1 py-1  w-[90%] border border-gray-500 rounded-lg text-3xl"
-            onChange={(e)=>setName(e.target.value)}
+            onChange={(e) => setName(e.target.value)}
             placeholder="Nazwa"
           />
           <input
             type="text"
             className="bg-gray-100 mb-8 px-1 py-1 w-[90%] border border-gray-500 rounded-lg text-3xl"
-            onChange={(e)=>setMail(e.target.value)}
+            onChange={(e) => setMail(e.target.value)}
             placeholder="E-mail"
           />
           <button
@@ -95,7 +96,43 @@ const NewUserHome = () => {
           </button>{" "}
         </form>
       )}
+      {step === 2 && (
+        <form className="flex flex-col justify-center items-center">
+          <img src={calendar} alt="calendar" className="mb-10 h-32" />
+          <p className="text-3xl font-bold tracking-wider font-poppins">
+            Wybierz miesiąc który chcesz przeskanować{" "}
+          </p>
+          <p className="my-8 text-xl text-gray-500 font-poppins">
+            Zalecamy wybór bieżącego miesiąca rozliczeniowego.{" "}
+          </p>
+          <div className="flex flex-row just items-center text-2xl  font-poppins">
+            <label htmlFor="month" className="mr-5">Wybierz miesiąc:</label>
+            <select name="months" id="month" className="bg-gray-300 p-2 rounded-xl">
+              <option value="1" selected>Styczeń</option>
+              <option value="2">Luty</option>
+              <option value="3">Marzec</option>
+              <option value="4">Kwiecień</option>
+              <option value="5">Maj</option>
+              <option value="6">Czerwiec</option>
+              <option value="7">Lipiec</option>
+              <option value="8">Sierpień</option>
+              <option value="9">Wrzesień</option>
+              <option value="10">Październik</option>
+              <option value="11">Listopad</option>
+              <option value="12">Grudzień</option>
+            </select>
+          </div>
+          <button
+            className="bg-[#1A9367] text-white py-3 px-3 font-bold font-playFair rounded-xl text-xl"
+            onClick={handleAddAcc}
+            type="submit"
+          >
+            Skanuj
+          </button>{" "}
+        </form>
+      )}
       <button onClick={handlereturn}>wróc</button>
+      <button onClick={handleNextStep}>force next</button>
     </div>
   );
 };
