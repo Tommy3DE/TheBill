7f1909bd83f8c4e3969e2f8ffaabf7bf9d2ad5cb
TomaszKJK <84966413+Tommy3DE@users.noreply.github.com>
2024-04-03 19:24:55 +0200

    dalej poprzadki

--- src/loggedUser/ScanEmail/ScanEmail.tsx
+++ src/loggedUser/ScanEmail/ScanEmail.tsx
@@ -1,58 +1,42 @@
 import { Link } from "react-router-dom";
 import SlimNav from "../../layout/SlimNav";
 import ReturnBtn from "../../components/ReturnBtn";
+import mail from "../../assets/iconsLogged/email.png";
 
 const ScanEmail = () => {
   return (
     <section>
       <SlimNav />
       <div className="mx-auto max-w-[1980px] ">
-      <div className="mt-24 lg:mx-[10%] mx-[2%]">
-        <div className="flex flex-row justify-start items-center font-black">
-          <h1 className="text-4xl">Wybierz skrzynkę którą chcesz skanować</h1>
-        </div>
-        <div className="px-4 py-2 my-6 bg-gray-300 rounded-lg">
-          Poniżej wyświetlone są adresy e-mail, połączone z aplikacją. W tabeli
-          możesz znaleźć datę ostatniego skanowania, oraz jaki jest aktualny
-          status skrzynki. Jeżeli chcesz rozpocząć skanowanie skrzynki w
-          poszukiwaniu dokumentów, naciśniej zielony przycisk “skanuj”.
-        </div>
-        <div className="w-full flex flex-col">
-          <div className="lg:p-4 border-green-700 rounded-xl lg:h-72 w-full border-4 ">
-            <ul className="flex justify-around lg:text-2xl font-bold flex-row list-none w-full border-b-4 border-green-700">
-              <li className="text-center border-r-4 border-green-700 w-1/4">
-                Konto
-              </li>
-              <li className="text-center border-r-4 border-green-700 w-1/4">
-                Ostatnie Skanowanie
-              </li>
-              <li className="text-center border-r-4 border-green-700 w-1/4">
-                Akcja
-              </li>
-              <li className="text-center w-1/4">Status</li>
-            </ul>
-            <div className="w-full flex flex-row lg:text-xl">
-              <div className="w-1/4 lg:p-2 border-green-700 border-r-4 text-center">
-                test@test.pl
+        <div className="mt-36 lg:mx-[10%] mx-[2%] font-poppins text-center">
+          <h1 className="text-4xl font-black">
+            W tym panelu możesz rozpocząć skanowanie swojej skrzynki e-mail
+          </h1>
+          <h2 className="text-2xl ">
+            Skanowanie rozpocznie się od momentu, w którym zakończyło się
+            ostatnie skanowanie.{" "}
+          </h2>
+          <div className="bg-gray-400 p-4 lg:w-1/3 mx-auto rounded-3xl">
+            <div className="bg-white flex flex-row justify-center items-center rounded-3xl px-4 py-2">
+              <img src={mail} alt="mail" />
+              <h1 className="text-2xl font-black ml-2">Cezary1998@gmail.com</h1>
+            </div>
+            <div className="bg-white flex flex-col justify-between items-center rounded-3xl px-4 py-2 mt-5 text-small">
+              <div>
+                <span>Ostatnie skanowanie:</span>
+                <span></span>
               </div>
-              <div className="w-1/4 p-2 border-green-700 border-r-4 text-center">
-                2/12/2023
-              </div>
-              <div className="w-1/4 p-2 border-green-700 border-r-4 text-center">
-                <Link to={'/logged/scanMail/scanPeriod'}>
-                <button className="bg-green-400 text-bold rounded-lg py-1 lg:w-1/3">
-                  Skanuj
-                </button>
-                </Link>
-              </div>
-              <div className="w-1/4 p-2 border-green-700 font-bold text-green-500 text-center">
-                Aktywna
-              </div>
+              <div>Liczba ostatnio pobranych faktur:</div>
             </div>
           </div>
+          <Link to={"/logged/scanMail/scanPeriod"}>
+            <button className="bg-green-400 text-bold rounded-lg py-1 lg:w-1/3">
+              Skanuj
+            </button>
+          </Link>
+
+          <ReturnBtn route="/logged" />
         </div>
-      </div>
-      <ReturnBtn route='/logged'/>
       </div>
     </section>
   );
