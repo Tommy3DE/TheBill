cc46a0edd8b2c7ff69588d25e01528d3c4c97c34
TomaszKJK <84966413+Tommy3DE@users.noreply.github.com>
2024-04-26 23:07:55 +0200

    final scana

--- src/loggedUser/ScanEmail/ScanPeriod.tsx
+++ src/loggedUser/ScanEmail/ScanPeriod.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import SlimNav from "../../layout/SlimNav";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useUserData } from "../../context/UserDataContext";
 import { InvoiceType } from "../documents/components/MthPage";
 import locked from "../../assets/locked 1.png";
 import { AiOutlineCloseCircle } from "react-icons/ai";
 import { FaRegEye } from "react-icons/fa";
 import { toast } from "react-toastify";
+import ReturnBtn from "../../components/ReturnBtn";
 
 const ScanPeriod = () => {
   const [date, setDate] = useState("");
@@ -21,7 +22,6 @@ const ScanPeriod = () => {
   const [invoiceToDelete, setInvoiceToDelete] = useState<number | null>(null);
 
   const accessToken = localStorage.getItem("accessToken");
-
 
   const handleImageClick = (thumbnail: string) => {
     setSelectedImage(thumbnail);
@@ -61,8 +61,8 @@ const ScanPeriod = () => {
       })
       .then(() => {
         setInvoices((prevInvoices) =>
-        prevInvoices.filter((invoice) => invoice.id !== invoiceToDelete)
-      );
+          prevInvoices.filter((invoice) => invoice.id !== invoiceToDelete)
+        );
         toast.success("Faktura usunięta pomyślnie!", {
           position: "top-right",
           autoClose: 5000,
@@ -72,9 +72,8 @@ const ScanPeriod = () => {
           draggable: true,
           progress: undefined,
         });
-        
+
         hideModal();
-      
       })
       .catch((error) => {
         console.error("Error:", error);
@@ -200,75 +199,104 @@ const ScanPeriod = () => {
     <section className=" font-poppins">
       <SlimNav />
       <div className="mx-auto max-w-[1980px] ">
-      {selectedImage && (
-            <div
-              className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-10"
-              onClick={handleCloseModal}
-            >
-              <img
-                src={`data:image/jpeg;base64,${selectedImage}`}
-                alt="Enlarged thumbnail"
-                className="max-w-3/4 max-h-3/4"
-                style={{
-                  width: "auto",
-                  height: "auto",
-                  maxWidth: "75%",
-                  maxHeight: "75%",
-                }}
-              />
-            </div>
-          )}
+        {selectedImage && (
+          <div
+            className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-10"
+            onClick={handleCloseModal}
+          >
+            <img
+              src={`data:image/jpeg;base64,${selectedImage}`}
+              alt="Enlarged thumbnail"
+              className="max-w-3/4 max-h-3/4"
+              style={{
+                width: "auto",
+                height: "auto",
+                maxWidth: "75%",
+                maxHeight: "75%",
+              }}
+            />
+          </div>
+        )}
         {!loading ? (
           nextStep ? (
-            <div className=" lg:mt-[7%] mt-[25%] flex flex-row flex-wrap justify-center items-center ">
-              {invoices.map((invoice) => (
-                <div
-                  key={invoice.id}
-                  className="relative group m-4"
-                  style={{ width: "260px", height: "400px" }}
-                >
-                  <img
-                    src={`${
-                      invoice.thumbnail.length > 0
-                        ? `data:image/jpeg;base64,${invoice.thumbnail}`
-                        : locked
-                    } `}
-                    alt="Thumbnail"
-                    className={`border-2 my-2 p-1 w-full ${
-                      invoice.thumbnail.length > 0 ? "h-full object-cover" : ""
-                    }`}
-                  />
-                  <div
-                    className={`absolute inset-0  justify-center items-center hidden group-hover:flex mt-2 ${
-                      invoice.thumbnail.length > 0 ? "-mb-2" : "mb-32"
-                    } `}
-                    style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
-                  >
-                    <div className="flex space-x-4">
-                      <AiOutlineCloseCircle
-                        className="text-white bg-red-500 rounded-full cursor-pointer"
-                        style={{
-                          padding: "0.25rem",
-                          height: "40px",
-                          width: "40px",
-                        }}
-                        onClick={(e: React.MouseEvent<SVGElement>) =>
-                          handleIconClick(e, invoice.id)
-                        }
+            <div>
+              {invoices.length > 0 && (
+                <p className="text-2xl mx-[5%] text-center text-gray-600 leading-loose lg:mt-[7%] mt-[25%] tracking-wider">
+                  Poniżej, wyświetlone zostały faktury znalezione na Twojej
+                  skrzynce.<br/> Zdecyduj, czy któreś z nich chcesz usunąć – jeżeli
+                  nie, to wybierz <span className="text-gray-800 font-bold">ZATWIERDŹ</span>
+                </p>
+              )}
+              <div className="  flex flex-row mt-5 flex-wrap justify-center items-center ">
+                {invoices.length > 0 ? (
+                  invoices.map((invoice) => (
+                    <div
+                      key={invoice.id}
+                      className="relative group m-4"
+                      style={{ width: "260px", height: "400px" }}
+                    >
+                      <img
+                        src={`${
+                          invoice.thumbnail.length > 0
+                            ? `data:image/jpeg;base64,${invoice.thumbnail}`
+                            : locked
+                        } `}
+                        alt="Thumbnail"
+                        className={`border-2 my-2 p-1 w-full ${
+                          invoice.thumbnail.length > 0
+                            ? "h-full object-cover"
+                            : ""
+                        }`}
                       />
-                      <FaRegEye
-                        className="text-blue-500 cursor-pointer bg-white rounded-full"
-                        style={{
-                          padding: "0.25rem",
-                          height: "40px",
-                          width: "40px",
-                        }}
-                        onClick={() => handleImageClick(invoice.thumbnail)}
-                      />
+                      <div
+                        className={`absolute inset-0  justify-center items-center hidden group-hover:flex mt-2 ${
+                          invoice.thumbnail.length > 0 ? "-mb-2" : "mb-32"
+                        } `}
+                        style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+                      >
+                        <div className="flex space-x-4">
+                          <AiOutlineCloseCircle
+                            className="text-white bg-red-500 rounded-full cursor-pointer"
+                            style={{
+                              padding: "0.25rem",
+                              height: "40px",
+                              width: "40px",
+                            }}
+                            onClick={(e: React.MouseEvent<SVGElement>) =>
+                              handleIconClick(e, invoice.id)
+                            }
+                          />
+                          <FaRegEye
+                            className="text-blue-500 cursor-pointer bg-white rounded-full"
+                            style={{
+                              padding: "0.25rem",
+                              height: "40px",
+                              width: "40px",
+                            }}
+                            onClick={() => handleImageClick(invoice.thumbnail)}
+                          />
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-              ))}
+                  ))
+                ) : (
+                  <p className="text-3xl text-center my-64 text-gray-400">
+                    Brak faktur
+                  </p>
+                )}
+              </div>
+              <div className="flex lg:flex-row justify-center my-16 flex-col">
+                <ReturnBtn route="/logged" />
+                {invoices.length > 0 && (
+                  <Link to={`/logged/documents/${selectedPeriod}2024`}>
+                    <>
+                      <button className="uppercase ml-5 font-playFair text-3xl font-black text-center items-center text-white bg-green-400 px-10 py-4 rounded-2xl hover:bg-green-500">
+                        Zatwierdź{" "}
+                      </button>
+                    </>
+                  </Link>
+                )}
+              </div>
             </div>
           ) : (
             <div className="mt-36 lg:mx-[10%] mx-[2%] flex flex-col justifty-center items-center">
@@ -319,12 +347,14 @@ const ScanPeriod = () => {
                   <option value={12}>Grudzień</option>
                 </select>
               </div>
-              {selectedPeriod !== '' && <button
-                className="mt-16 text-3xl bg-green-500 p-3 rounded-lg text-white w-1/5 text-center hover:scale-105 cursor-pointer"
-                onClick={handleCombinedScan}
-              >
-                Skanuj
-              </button>}
+              {selectedPeriod !== "" && (
+                <button
+                  className="mt-16 text-3xl bg-green-500 p-3 rounded-lg text-white w-1/5 text-center hover:scale-105 cursor-pointer"
+                  onClick={handleCombinedScan}
+                >
+                  Skanuj
+                </button>
+              )}
             </div>
           )
         ) : (
@@ -347,7 +377,9 @@ const ScanPeriod = () => {
             onClick={(e) => e.stopPropagation()}
           >
             <h2 className="font-bold text-3xl">Potwierdzenie</h2>
-            <p className='my-5 text-lg'>Czy na pewno chcesz usunąć tę fakturę?</p>
+            <p className="my-5 text-lg">
+              Czy na pewno chcesz usunąć tę fakturę?
+            </p>
             <div className="flex justify-around mt-4">
               <button
                 className="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-700 text-xl"
