2654eafb42d41278dcfb34a429fb1e9d2efb526a
TomaszKJK <84966413+Tommy3DE@users.noreply.github.com>
2024-04-29 14:48:35 +0200

    cleanup

--- src/loggedUser/history/History.tsx
+++ src/loggedUser/history/History.tsx
@@ -1,12 +1,10 @@
 import SlimNav from "../../layout/SlimNav"
-import HistoryRoutes from "./components/HistoryRoutes"
 
 const History = () => {
   return (
     <div className="h-auto lg:h-screen w-full">
     <SlimNav/>
     <div className="mx-auto max-w-[1980px] ">
-    <HistoryRoutes/>
     </div>
     </div>
   )
