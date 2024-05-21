4b05d1cef1924c4c50f96dcdc9090958c1d61aea
TomaszKJK <84966413+Tommy3DE@users.noreply.github.com>
2024-05-21 16:11:24 +0200

    #169 - autoscan zmieniony

--- src/loggedUser/AutoScan.tsx
+++ src/loggedUser/AutoScan.tsx
@@ -14,46 +14,19 @@ const AutoScan = () => {
         <h2 className="text-xl text-gray-700 text-center tracking-wider">
           W tym panelu możesz ustawić automatyczne skanowanie swojej skrzynki.
           <br />
-          Wybierz dzień, w którym ma odbyć się skanowanie.
+          Wybierz częstotliwość, z którą ma odbywać się skanowanie.
         </h2>
         <h1 className="lg:text-4xl text-3xl text-center my-10 font-bold">
-          Skanowanie ma się odbywać każdego{" "}
+          Skanowanie ma się odbywać:
           <span>
             <select className="bg-gray-200 rounded">
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-              <option value="11">11</option>
-              <option value="12">12</option>
-              <option value="13">13</option>
-              <option value="14">14</option>
-              <option value="15">15</option>
-              <option value="16">16</option>
-              <option value="17">17</option>
-              <option value="18">18</option>
-              <option value="19">19</option>
-              <option value="20">20</option>
-              <option value="21">21</option>
-              <option value="22">22</option>
-              <option value="23">23</option>
-              <option value="24">24</option>
-              <option value="25">25</option>
-              <option value="26">26</option>
-              <option value="27">27</option>
-              <option value="28">28</option>
-              <option value="29">29</option>
-              <option value="30">30</option>
-              <option value="31">31</option>
+              <option value="1">codziennie</option>
+              <option value="2">co tygodzień</option>
+              <option value="3">co 2 tygodnie</option>
+              <option value="4">na koniec miesiąca</option>
+              
             </select>
           </span>{" "}
-          dnia miesiąca.
         </h1>
         <h2 className="text-xl text-gray-700 text-center tracking-wider">
           Po zakończonym skanowaniu, prześlemy do Ciebie maila, że gotowy skan
