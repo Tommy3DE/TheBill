704c0e11560f69f939ef7e06619fd6b8e6f4c763
TomaszKJK <84966413+Tommy3DE@users.noreply.github.com>
2024-06-25 11:57:04 +0200

    #257 zmiana

--- src/layout/pages/Oferta/EasyBanner.tsx
+++ src/layout/pages/Oferta/EasyBanner.tsx
@@ -6,7 +6,7 @@ const EasyBanner = () => {
         Proste zarządzanie kosztami 
         </h1>
         <h3 className='tracking-wider text-xl font-poppins w-2/3 text-center'>
-        Bez względu na rodzaj działalności jaki prowadzisz, oferujemy Ci produkt, który <span className='font-black text-[#1A9367] '>zautomatyzuje comiesięczne zbieranie dokumentów kosztowych</span>  i usprawni kontakt z Twoim biurem księgowym. Przetestuj nasz produkt podczas bezpłatnego 30-dniowego okresu próbnego. 
+        Bez względu na rodzaj działalności jaki prowadzisz, oferujemy Ci produkt, który <span className='font-black text-[#1A9367] '>zautomatyzuje comiesięczne zbieranie dokumentów kosztowych</span>  i usprawni kontakt z Twoim biurem księgowym. Przetestuj nasz produkt podczas bezpłatnego 14-dniowego okresu próbnego. 
         </h3>
     </div>
   )
